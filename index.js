9061bbae074abd633da1d43c2e03d8f8bb24bfe3
ExcelE <37338625+ExcelE@users.noreply.github.com>
2018-04-06 13:47:30 -0400

    added static images

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ app.use(bodyParser.json());
 
 routes(app);
 
+// service static files 
+app.use(express.static('public'));
+
 app.get('/', (req,res) =>
     res.send(`Node and EXPRESS server is running on port ${PORT}`)
 );
