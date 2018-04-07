bfa14492a16b6137a84afcc23f8b1c77cc897d78
ExcelE <37338625+ExcelE@users.noreply.github.com>
2018-04-07 15:40:51 -0400

    second

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 
 const app = express();
-const PORT = 3000;
+const PORT = 2000;
 
 //mongoose setup
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/CRMdb', {
     useMongoClient: true
 });
+
+///////
+
+
+//////
 
 //bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,9 +26,9 @@ routes(app);
 // service static files 
 app.use(express.static('public'));
 
-app.get('/', (req,res) =>
+app.get('/', (req,res) =>{
     res.send(`Node and EXPRESS server is running on port ${PORT}`)
-);
+});
 
 // logger
 
